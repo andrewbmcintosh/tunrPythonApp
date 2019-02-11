cafa844cbc766ca9a02c92d4f29cb1bec20c89c8
andrewmcintosh <abmcintosh7@gmail.com>
2019-02-11 10:44:28 -0500

    first commit

---
+++ tunr/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+from .models import Artist, Song
+
+def artist_index(request):
+    context = {'artists': Artist.objects.all()}
+    return render(request, 'tunr/artist_index.html', context)
+
+def song_list(request):
+    context = {'songs': Song.objects.all()}
+    return render(request, 'tunr/song')
